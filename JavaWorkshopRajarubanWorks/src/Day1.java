075e4d235ec07aa8876ad10956c8cdffce81f78d
Rajaruban Rajindram <85555894+rubanero14@users.noreply.github.com>
2024-02-15 01:03:31 +0800

    added extra class

---
+++ JavaWorkshopRajarubanWorks/src/Day1.java
@@ -0,0 +1,32 @@
+public class Day1 extends Day1_Rajaruban_Works {
+	public static void main(String[] args) {
+//		Day1_Rajaruban_Works d1 = new Day1_Rajaruban_Works();
+//		
+//		d1.x = 2;
+//		d1.y = 3;
+//		
+//		int x = d1.x;
+//		int y = d1.y;
+//		
+//		System.out.println("Sum of two numbers: " + sumOfTwoNumbers(x, y));
+//		
+//		swapTwoNumbers(x, y);
+//		
+//		System.out.println(printGrades(81));
+//		
+//		printTableOfNine();
+//		
+//		launchBrowserUsingSelenium("https:www.google.com");
+//		
+//		System.out.println(checkIsEvenOrOdd(99));
+//		
+//		double[] numArray = {5, 6, 4, 30, 1, 2, 0.5};
+//		System.out.println(findMinAndMax(numArray));	
+//		
+//		System.out.println(checkIsPalindrome("kayak"));
+//		
+		long factorial = 10;
+		long result = factorialCalculator(factorial);
+		System.out.println("Factorial for " + factorial + " is " + result);	
+	}
+}
